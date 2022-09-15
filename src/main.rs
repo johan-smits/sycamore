6ba346ae674c52c0f3e265561a86ac7fa882afed
Johan Smits <johan.smits@leftclick.eu>
2022-09-15 16:46:17 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::rc::Rc;
+use sycamore::prelude::*;
+
+#[derive(Prop)]
+struct Props {
+    input: Rc<String>,
+}
+
+#[component]
+fn OtherComponent<G: Html>(cx: Scope, props: Props) -> View<G> {
+
+
+    view! { cx,
+        p {
+            (props.input)
+        }
+    }
+}
+
+#[component]
+fn App<G: Html>(cx: Scope) -> View<G> {
+    let input = create_signal(cx, String::new());
+
+    view! { cx,
+        p {
+            input(bind:value=input, type="email")
+        }
+        OtherComponent(input=input.get())
+    }
+}
+
+fn main() {
+    sycamore::render(|cx| {
+        view! { cx, App {} }
+    });
+}
