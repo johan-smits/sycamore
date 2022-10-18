3b714fec82f45702cdbb116c216c6a8cc717ce03
Johan Smits <johan.smits@leftclick.eu>
2022-10-18 16:19:29 +0200

    SVG viewbox must be with a capital B (closes: sycamore-rs/sycamore#488)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     sycamore::render(|cx| {
         view! { cx,
             h1 {"SVG size test"}
-            svg(xmlns="http://www.w3.org/2000/svg", aria-label="Flickr", role="img", viewbox="0 0 512 512") {
+            svg(xmlns="http://www.w3.org/2000/svg", aria-label="Flickr", role="img", viewBox="0 0 512 512") {
                 rect(width="512", height="512", rx="15%", fill="#fff") {}
                 circle(cx="157", cy="256", fill="#0063dc", r="79") {}
                 circle(cx="355", cy="256", fill="#ff0084", r="79") {}
